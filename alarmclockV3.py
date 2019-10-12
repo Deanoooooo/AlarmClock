a23f68b805d2b8dcced7881180392d79bb912a8f
Deanoooooo <56487347+Deanoooooo@users.noreply.github.com>
2019-10-12 13:05:52 -0600

    Add files via upload

--- alarmclockV3.py
+++ alarmclockV3.py
@@ -26,11 +26,30 @@ def getTimeAndDate():
 	md = month + " " + day
 	return (t, md)
 
+nothing = 1
+a = datetime.now()
+(t, md) = getTimeAndDate()
+tHour = int(a.strftime("%H"))
 
-while(True):
+T= StringVar()
+MD= StringVar()
+T.set(t)
+MD.set(md)
+displayTime =  Label(root, textvariable=T, font=("Verdana",112), fg="#e6e6e6", bg="#e6e6e6")
+displayDay = Label(root, textvariable=MD, font=("Verdana",60), fg="#e6e6e6", bg="#e6e6e6")
+
+displayTime.place(relx=0.5, rely=0.4, anchor='n')
+displayDay.place(relx=0.5, rely=0.55, anchor='n')
+
+while (True):
 	a = datetime.now()
 	(t, md) = getTimeAndDate()
 	tHour = int(a.strftime("%H"))
+
+	T= StringVar()
+	MD= StringVar()
+	T.set(t)
+	MD.set(md)
 
 	if tHour >= 6 and tHour <= 8:
 		hrcolor = "#707fd1"
@@ -44,20 +63,10 @@ while(True):
 		hrcolor = "#273270"
 	elif tHour == 23 or tHour == 24 or (tHour >= 0 and tHour <= 5):
 		hrcolor = "#1d2450"
-	root.configure(background=hrcolor)
-	
-	T= StringVar()
-	MD= StringVar()
-	T.set(t)
-	MD.set(md)
+
 	displayTime =  Label(root, textvariable=T, font=("Verdana",112), fg="#e6e6e6", bg=hrcolor)
 	displayDay = Label(root, textvariable=MD, font=("Verdana",60), fg="#e6e6e6", bg=hrcolor)
-	displayTime.pack()
-	displayDay.pack()
-	#displayTime.place(relx=0.5, rely=0.4, anchor='n')
-	#displayDay.place(relx=0.5, rely=0.55, anchor='n')
-	root.after(500, root.update())
-	
-
+	root.configure(background=hrcolor)
+	root.after(500, nothing=1)
 		
 
